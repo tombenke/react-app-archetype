8044ca5299fc9f74055d6be8e2f030c0a9973187
Tamás Benke <tombenke@gmail.com>
2019-01-01 15:43:22 +0100

    Add kickoff config files and scripts

--- .kickoff.sed
+++ .kickoff.sed
@@ -3,5 +3,3 @@ s/A template project for react frontends/{{package_description}}/g
 s/Tamás Benke/{{author_full_name}}/g
 s/tombenke@gmail.com/{{author_email}}/g
 s/"https:\/\/github\.com\/tombenke/"https:\/\/github\.com\/{{author_github_account}}/g
-s/TomBenke/{{brand_name}}/g
-s/https:\/\/tombenke.github.io\/me\//{{brand_uri}}/g
--- .kickoff.test.sh
+++ .kickoff.test.sh
@@ -3,12 +3,13 @@ testBaseDir=${HOME}/tmp
 destDir=${testBaseDir}/test-react-app
 cwd=`pwd`
 echo Executing kickoff -a .kickoff.test.yml -f . -d destDir
-rm $destDir -r
+rm $destDir -rf
 kickoff -a .kickoff.test.yml -f . -d $destDir
 cd $destDir
+echo $destDir
+pwd
 ./.kickoff.sh
-npm install
-npm run build
+npm install && npm run build
 echo "Start a web server (such as http-server) in the dist folder listening on the right port"
 echo "then open the app in a browser to test"
 # npm run test
