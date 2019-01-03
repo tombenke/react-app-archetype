90fa7b842050778b076931cedeee38805a7b00dd
Tamás Benke <tombenke@gmail.com>
2019-01-03 19:46:15 +0100

    Add Router to the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import { Logo } from './components/Logo/'
 import * as serviceWorker from './serviceWorker'
+import { configureStore } from '@tombenke/redux-app'
+//
+// Create the store
+const store = configureStore()
 
-ReactDOM.render(<Logo />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <HashRouter>
+            <Switch>
+                <Route path="/" name="Logo" component={Logo} />
+            </Switch>
+        </HashRouter>
+    </Provider>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
